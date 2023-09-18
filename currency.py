9cbd10de7aae67c084f921978847a0b2ad351deb
Degalmadi <145328365+Degalmadi@users.noreply.github.com>
2023-09-18 15:28:30 +0530

    Add files via upload

---
+++ currency.py
@@ -0,0 +1,26 @@
+from currency_converter import CurrencyConverter
+import tkinter as tk
+a = CurrencyConverter()
+window= tk. Tk()
+window.geometry("580x360")
+
+def clicked():
+    amount = int(e1.get())
+    curl = e2.get()
+    cur2 = e3.get()
+    data = a.convert(amount, cur1, cur2)
+    l5 = tk .Label(window,text= data,font= "Times 25 bold").place(x = 200, y = 290)
+
+l1 = tk.Label(window,text="Currency Converter", font= "Times 25 bold").place(x = 100, y = 30)
+l2 = tk.Label(window,text="Currency Converter", font= "Times 18 bold").place(x = 50, y = 80)
+e1 = tk.Entry(window)
+
+l3 = tk.Label(window,text="Currency Converter", font= "Times 18 bold").place(x = 50, y = 130)
+e2 = tk.Entry(window).place(x = 300, y = 140)
+l4 = tk.Label(window,text="Currency Converter", font= "Times 18 bold").place(x = 50, y = 180)
+e3 = tk.entry(window).place(x = 300, y = 190)
+
+b1 = tk.Button(window,text="click", command=clicked).place(x = 230, y = 240)
+e1.place(x = 300, y = 90)
+e2.place(x = 300, y = 140)
+window.mainloop()
